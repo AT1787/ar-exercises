367fb8d4ea968cf9cc4a85e97ac1ba0c45bf44c6
Andrew Trac <andrewtrac87@hotmail.com>
2020-06-22 23:58:55 +0000

    Completed exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+puts @store1.name
+puts @store2.annual_revenue
+
+@store1.update(name: 'Yaletown')
+puts @store1.name
+
